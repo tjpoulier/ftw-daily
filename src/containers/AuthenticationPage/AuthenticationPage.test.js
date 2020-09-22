f85fafe6bfa551077ab44fed01d0a24d8304c639
Jenni Laakso <jenni@sharetribe.com>
2020-09-22 13:58:01 +0300

    AuthenticationPage: add login with Facebook option

--- src/containers/AuthenticationPage/AuthenticationPage.test.js
+++ src/containers/AuthenticationPage/AuthenticationPage.test.js
@@ -6,7 +6,7 @@ import { AuthenticationPageComponent } from './AuthenticationPage';
 const noop = () => null;
 
 describe('AuthenticationPageComponent', () => {
-  it('matches snapshot', () => {
+  it('matches snapshot when social logins are not shown', () => {
     const props = {
       history: { push: noop },
       location: { state: { from: '/protected' } },
@@ -22,6 +22,29 @@ describe('AuthenticationPageComponent', () => {
       intl: fakeIntl,
       sendVerificationEmailInProgress: false,
       onResendVerificationEmail: noop,
+      showSocialLoginsForTests: false,
+    };
+    const tree = renderShallow(<AuthenticationPageComponent {...props} />);
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('matches snapshot when social logins are shown', () => {
+    const props = {
+      history: { push: noop },
+      location: { state: { from: '/protected' } },
+      tab: 'login',
+      isAuthenticated: false,
+      authInProgress: false,
+      scrollingDisabled: false,
+      currentUserHasListings: false,
+      onLogout: noop,
+      onManageDisableScrolling: noop,
+      submitLogin: noop,
+      submitSignup: noop,
+      intl: fakeIntl,
+      sendVerificationEmailInProgress: false,
+      onResendVerificationEmail: noop,
+      showSocialLoginsForTests: true,
     };
     const tree = renderShallow(<AuthenticationPageComponent {...props} />);
     expect(tree).toMatchSnapshot();
